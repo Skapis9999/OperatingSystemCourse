d77dcedac3366b8bd03f82e0f2ccef5efbb0aaca
Christos Skapetis <45330616+Skapis9999@users.noreply.github.com>
2021-02-16 21:10:34 +0200

    Add files via upload

---
+++ quiz6/pageFaults.m
@@ -0,0 +1,27 @@
+﻿function status = pageFaults(pageReq, nFrame)
+
+
+    persistent frames = zeros(nFrame, 1);
+    persistent times = zeros(nFrame, 1);
+    
+    found=0;        %flag
+    for j=1:nFrame
+        if(frames(j)==pageReq)
+            found=1;        %flag found positive
+            times(j)=time;
+            break;
+        end
+    end
+    
+    if(found==1)
+        status = 'H';   
+        return
+    end
+    status='M';
+    [M,I] = min(times);     %min and index of min
+    times(I) = time;
+    frames(I) = pageReq;
+    
+end
+
+
